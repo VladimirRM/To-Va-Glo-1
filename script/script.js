3645e6501e897754057e50d287b80bd6b575939d
Vladimir RM <shvdrummer@gmail.com>
2022-11-11 11:57:08 +0200

    add todo

--- script/script.js
+++ script/script.js
@@ -1,13 +1,16 @@
 let addMessage = document.querySelector(".message"),
-  addButton = document.querySelector(".add");
+  addButton = document.querySelector(".add"),
+  todo = document.querySelector(".todo");
 
-  let todoList = [];
+let todoList = [];
 
 addButton.addEventListener("click", function () {
-      let newTodo = {
-        todo: addMessage.value,
-        checked: false,
-        important:false
-      }
-      console.log(newTodo);
+  let newTodo = {
+    todo: addMessage.value,
+    checked: false,
+    important: false,
+  };
+
+  todoList.push(newTodo);
+  console.log(todoList);
 });
