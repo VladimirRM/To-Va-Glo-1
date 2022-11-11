c2bc9c5ef5ce82cbb0d76ebf4a73aa9c0b271966
Vladimir RM <shvdrummer@gmail.com>
2022-11-11 11:52:52 +0200

    add array totoList

--- script/script.js
+++ script/script.js
@@ -1,8 +1,13 @@
 let addMessage = document.querySelector(".message"),
   addButton = document.querySelector(".add");
 
+  let todoList = [];
+
 addButton.addEventListener("click", function () {
       let newTodo = {
-        
+        todo: addMessage.value,
+        checked: false,
+        important:false
       }
+      console.log(newTodo);
 });
